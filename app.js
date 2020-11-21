8313b968b75a2dd5384b4dfd183457f2cff254d9
TANISHQ SOIN <50992787+TanishqSoin11@users.noreply.github.com>
2020-11-21 20:50:12 +0530

    Initial Commit

---
+++ app.js
@@ -0,0 +1,42 @@
+// importing core modules
+const http = require('http');
+const path = require('path')
+
+//importing npm modules
+const express = require('express');
+const bodyParser = require('body-parser')
+
+// importing path.js
+const rootDir= require('./util/path')
+
+// creating express() object
+const app = express();
+
+// Setting Templating engine => pug
+app.set('view engine', 'pug')
+app.set('views','views')
+
+// importing routes
+const adminData = require('./routes/admin')
+const shopRoutes = require('./routes/shop');
+
+// body-parser
+app.use(bodyParser.urlencoded({extended: false}))
+
+// importing public folder
+app.use(express.static(path.join(__dirname,'public')))
+
+// routing
+app.use('/admin',adminData.routes)
+app.use(shopRoutes)
+
+// setting a route to 404.html
+app.use('/',(req, res, next)=>{
+    res.status(404).sendFile(path.join(rootDir,'views','404.html'))
+})
+
+// event listener
+app.listen(3000)
+
+
+
---
+++ routes/shop.js
@@ -0,0 +1,25 @@
+// importing core modules
+const path = require('path')
+
+// importing express
+const express = require('express')
+
+// importing util
+const rootDir = require('../util/path')
+
+// importing adminData from routes
+const adminData = require('./admin')
+
+// creating an express router object 
+const router = express.Router()
+
+// / =>GET
+router.get('/',(req, res, next) => {
+    // passing adminData.products to shop.pug 
+    // and rendering shop.pug for incoming requests
+    const products = adminData.products
+    res.render('shop', {prods: products, docTitle: 'Shop'})
+})
+
+// exports
+module.exports = router
